dbf8e54462b07b365b7da765f5f120ce9850274a
Inácio <Inaski.program@gmail.com>
2025-02-15 19:03:51 -0300

    error solved?

--- r2a/r2ampegdash.py
+++ r2a/r2ampegdash.py
@@ -11,17 +11,16 @@ class R2AMpegDash(IR2A):
         self.throughputs = []
         self.request_time = 0
         self.qi = []
-
+        self.lastT = 0
+        self.nowT = 0
         self.delta_min = 0.05
         self.k = 21
         self.p0 = 0.2
         self.EstimatedT = 1
         self.delta = 1
-        self.p=None
-        self.mi = 0.2
-        self.lastT = 0
+        self.p=0
+        self.parsed_mpd = None
         
-
 
     def handle_xml_request(self, msg):
         self.request_time = time.perf_counter()
@@ -29,12 +28,11 @@ class R2AMpegDash(IR2A):
 
 
     def handle_xml_response(self, msg):
-
-        parsed_mpd = parse_mpd(msg.get_payload())
-        self.qi = parsed_mpd.get_qi()
-
         t = time.perf_counter() - self.request_time
         self.throughputs.append(msg.get_bit_length() / t)
+        parsed_mpd = parse_mpd(msg.get_payload())
+        self.parsed_mpd = parsed_mpd
+        self.qi = parsed_mpd.get_qi()
         self.lastT = self.throughputs[-1]
 
         self.send_up(msg)
@@ -42,14 +40,16 @@ class R2AMpegDash(IR2A):
 
     def handle_segment_size_request(self, msg):
         self.request_time = time.perf_counter()
+        self.nowT = msg.get_bit_length()
+
         self.calcP()
         self.calcDelta()
         self.EstimatedT = self.estimate_throughput()
+        erre = 0.7* self.EstimatedT
         
-        current = (1-self.mi)*self.EstimatedT
         selected_qi = self.qi[0]
         for i in self.qi:
-            if current > i:
+            if erre > i:
                 selected_qi = i
 
 
@@ -59,7 +59,7 @@ class R2AMpegDash(IR2A):
 
     def handle_segment_size_response(self, msg):
         t = time.perf_counter() - self.request_time
-        self.throughputs.append(msg.get_bit_length() / t)
+        self.throughputs.append(msg.get_bit_length()/t)
         self.send_up(msg)
 
     def initialize(self):
